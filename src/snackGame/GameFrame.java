bf470e2c046697f22356ad8ae520469b2ff57e10
Amita Putry Prasasti <73166752+womenincode@users.noreply.github.com>
2022-01-05 12:04:03 +0700

    Add

--- src/snackGame/GameFrame.java
+++ src/snackGame/GameFrame.java
@@ -6,7 +6,7 @@ public class GameFrame extends JFrame {
 
     GameFrame(){
         this.add(new GamePanel());                              //mendeklarasikan object class GamePanel
-        this.setTitle("Snack Game - Created by : Kelompok 3");  //memberikan judul pada game panel
+        this.setTitle("Snack Game - Created by : Amita, Afifa, Raihan, Wahyu");  //memberikan judul pada game panel
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    //membuat tombol close untuk game panel
         this.setResizable(false);                               //mengatur ukuran panel tidak dapat di ubah dengan drag kursor
         this.pack();                                            //mengatur tata letak di dalam panel
